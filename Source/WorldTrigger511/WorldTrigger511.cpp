c1637fd3f8d90a9f04d9ec0333335ad6cb79e197
vitasanc <136711032+vitasanc@users.noreply.github.com>
2023-08-23 19:52:13 +0900

    c++のビルド

---
+++ Source/WorldTrigger511/WorldTrigger511.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WorldTrigger511.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WorldTrigger511, "WorldTrigger511" );
